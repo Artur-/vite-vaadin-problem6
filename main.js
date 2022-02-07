ab4cef3515fff09e5d9947ca40deda37b131a462
Artur Signell <artur@vaadin.com>
2022-02-07 14:28:53 +0200

    example

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import './style.css'
 
+import prismCss from 'prismjs/themes/prism-okaidia.css?inline';
+
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  ${prismCss}
 `
